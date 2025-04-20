c784db2941ed224355777e2818d4bac7a8fe9e23
midnight <384998447@qq.com>
2025-04-20 13:25:06 +0800

    初始化 greet grpc

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7194");
+
+var client = new Greeter.GreeterClient(channel);
+var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
